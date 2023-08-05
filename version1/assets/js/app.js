d631523348cd313ab1359c9943fabbaedb975513
gato365 <immanuelwilliams@gato365.com>
2023-08-05 13:50:42 -0700

    Change index and app to original files

--- version1/assets/js/app.js
+++ version1/assets/js/app.js
@@ -28,72 +28,20 @@ var solution = [
     "812945763"
 ]
 
-
-
-window.onload = function () {
-    // Restore board and errors from local storage
-    let savedBoard = localStorage.getItem('board');
-    let savedErrors = localStorage.getItem('errors');
-
-    if (savedBoard && savedErrors) {
-        board = JSON.parse(savedBoard);
-        errors = parseInt(savedErrors);
-    }
-
-    // Call setGame() to initialize the board
+window.onload = function() {
     setGame();
-
-    // Adding highlight event listeners after cells are created
-    let cells = document.querySelectorAll('.cell');
-
-    cells.forEach(cell => {
-        cell.addEventListener('mouseenter', highlight);
-        cell.addEventListener('mouseleave', removeHighlight);
-    });
-};
-
-function countDigits(board) {
-    // Initialize counters for digits 1-9
-    let counters = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0};
-
-    // Iterate through the board
-    for (let row of board) {
-        for (let digit of row) {
-            if (digit !== '-') {
-                counters[digit]++;
-            }
-        }
-    }
-
-    return counters;
 }
 
-
-
-
 function setGame() {
-      // Digits 1-9
-      for (let i = 1; i <= 9; i++) {
+    // Digits 1-9
+    for (let i = 1; i <= 9; i++) {
+        //<div id="1" class="number">1</div>
         let number = document.createElement("div");
-        number.id = i;
-        number.classList.add("selection-number");
-
-        let digit = document.createElement("span");
-        digit.innerText = i;
-        number.appendChild(digit);
-
-        // Count how many of this number is needed
-        let countNeeded = 9 * 9 - [...board.join('')].filter(x => x == i).length;
-        let count = document.createElement("span");
-        count.innerText = countNeeded;
-        count.classList.add("count");
-        number.appendChild(count);
-
-        // Attach click event listener
+        number.id = i
+        number.innerText = i;
         number.addEventListener("click", selectNumber);
-
+        number.classList.add("number");
         document.getElementById("digits").appendChild(number);
-        number.id = "";
     }
 
     // Board 9x9
@@ -112,114 +60,37 @@ function setGame() {
                 tile.classList.add("vertical-line");
             }
             tile.addEventListener("click", selectTile);
-            tile.classList.add("tile", "cell");
+            tile.classList.add("tile");
             document.getElementById("board").append(tile);
-            tile.id = 'cell-' + r.toString() + "-" + c.toString();
         }
     }
 }
 
-function selectNumber() {
-    let selectedNumber = parseInt(this.innerText);
-
-    if (isNaN(selectedNumber)) {
-        return;
+function selectNumber(){
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
     }
-
-    if (numSelected !== null) {
-        let previousSelected = document.getElementById(numSelected);
-        previousSelected.classList.remove("number-selected");
-    }
-
-    numSelected = selectedNumber;
-    this.classList.add("number-selected");
+    numSelected = this;
+    numSelected.classList.add("number-selected");
 }
-
-function updateNumberCount(num) {
-    // Get the number divs
-    let numberDivs = document.querySelectorAll(".selection-number");
-
-    for (let div of numberDivs) {
-        // Get the number from the first child (the digit span)
-        let digit = div.firstElementChild.innerText;
-
-        // If this is the div for the number we're looking for
-        if (digit === num) {
-            // Get the count span
-            let countSpan = div.querySelector('.count');
-
-            // Decrease the count
-            let currentCount = parseInt(countSpan.innerText);
-            countSpan.innerText = currentCount - 1;
-
-            // If count is zero, disable the number
-            if (currentCount - 1 == 0) {
-                div.classList.add('disabled');
-            }
-
-            // No need to check other divs
-            break;
-        }
-    }
-}
-
 
 function selectTile() {
-    if (numSelected == null) {
-        return;
-    }
+    if (numSelected) {
+        if (this.innerText != "") {
+            return;
+        }
 
-    if (this.innerText != "") {
-        return;
-    }
+        // "0-0" "0-1" .. "3-1"
+        let coords = this.id.split("-"); //["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
 
-    // "cell-0-0" "cell-0-1" .. "cell-3-1"
-    let coords = this.id.split("-").slice(1); //["0", "0"]
-    let r = parseInt(coords[0]);
-    let c = parseInt(coords[1]);
-
-    if (r < 0 || r >= solution.length || c < 0 || c >= solution[r].length) {
-        console.error(`Invalid coordinates: (${r}, ${c})`);
-        return;
-    }
-
-    if (solution[r][c] == numSelected) {
-        this.innerText = numSelected;
-        board[r] = board[r].substr(0, c) + numSelected.toString() + board[r].substr(c + 1);
-        updateNumberCount(numSelected);
-    }
-    else {
-        errors += 1;
-        document.getElementById("errors").innerText = errors;
+        if (solution[r][c] == numSelected.id) {
+            this.innerText = numSelected.id;
+        }
+        else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
+        }
     }
 }
-
-
-
-function startGame() {
-    // Clear errors
-    errors = 0;
-    document.getElementById('errors').textContent = '0';
-
-    // Reset board
-    let boardElement = document.getElementById("board");
-    boardElement.innerHTML = '';  // Clear board
-    let digitsElement = document.getElementById("digits");
-    digitsElement.innerHTML = ''; // Clear digits
-
-    // Save board and errors to local storage
-    localStorage.setItem('board', JSON.stringify(board));
-    localStorage.setItem('errors', errors);
-    // Reset timer
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    updateTimer();
-
-    // Reset strategy
-    document.getElementById('strategy').value = '';
-
-    // Hide strategies
-    document.getElementById('strategies-container').innerHTML = '';
-}
-
