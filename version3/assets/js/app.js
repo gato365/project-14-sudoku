945b5f378ec3847abad4a328e88018dae14f8169
gato365 <immanuelwilliams@gato365.com>
2023-08-08 20:25:39 -0700

    Still not decreasing

--- version3/assets/js/app.js
+++ version3/assets/js/app.js
@@ -47,41 +47,43 @@ window.onload = function() {
     });
 }
 
-// function setGame() {
-//     // Digits 1-9
-//     for (let i = 1; i <= 9; i++) {
-//         //<div id="1" class="number">1</div>
-//         let number = document.createElement("div");
-//         number.id = i
-//         number.innerText = i;
-//         number.addEventListener("click", selectNumber);
-//         number.classList.add("number");
-//         document.getElementById("digits").appendChild(number);
-//     }
+function setGame() {
+    const counts = getNumberCounts(board);
 
-//     // Board 9x9
-//     for (let r = 0; r < 9; r++) {
-//         for (let c = 0; c < 9; c++) {
-//             let tile = document.createElement("div");
-//             tile.id = r.toString() + "-" + c.toString();
-//             if (board[r][c] != "-") {
-//                 tile.innerText = board[r][c];
-//                 tile.classList.add("tile-start");
-//             }
-//             if (r == 2 || r == 5) {
-//                 tile.classList.add("horizontal-line");
-//             }
-//             if (c == 2 || c == 5) {
-//                 tile.classList.add("vertical-line");
-//             }
-//             tile.addEventListener("click", selectTile);
-//             tile.classList.add("tile", "cell");
-//             document.getElementById("board").append(tile);
-//             tile.id = 'cell-' + r.toString() + "-" + c.toString();
-//         }
-//     }
+    // Digits 1-9
+    for (let i = 1; i <= 9; i++) {
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerHTML = i + '<sup>' + (9 - counts[i]) + '</sup>'; // display count as superscript
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
 
-// }
+    // Board 9x9
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = 'cell-' + r.toString() + "-" + c.toString();
+            if (board[r][c] != "-") {
+                tile.innerText = board[r][c];
+                tile.classList.add("tile-start");
+            }
+            if (r == 2 || r == 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 2 || c == 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile", "cell");
+            document.getElementById("board").append(tile);
+        }
+    }
+
+
+    setInitialCounts();
+}
 
 function selectNumber(){
     if (numSelected != null) {
@@ -90,6 +92,34 @@ function selectNumber(){
     numSelected = this;
     numSelected.classList.add("number-selected");
 }
+
+// function selectTile() {
+//     if (numSelected) {
+//         if (this.innerText != "") {
+//             return;
+//         }
+
+//         // "0-0" "0-1" .. "3-1"
+//         let coords = this.id.split("-"); //["0", "0"]
+//         let r = parseInt(coords[1]);
+//         let c = parseInt(coords[2]);
+
+       
+        
+//         if (solution[r][c] == numSelected.id) {
+//             this.innerText = numSelected.id;
+//         }
+//         else {
+//             errors += 1;
+//             document.getElementById("errors").innerText = errors;
+//         }
+//     }
+// }
+
+
+
+// Timer Related Functions
+// Update the timer div with the current time
 
 function selectTile() {
     if (numSelected) {
@@ -102,22 +132,21 @@ function selectTile() {
         let r = parseInt(coords[1]);
         let c = parseInt(coords[2]);
 
-       
-        
         if (solution[r][c] == numSelected.id) {
             this.innerText = numSelected.id;
-        }
-        else {
+            updateNumberCount(numSelected.id); // Decrease the count and update display
+        } else {
             errors += 1;
             document.getElementById("errors").innerText = errors;
         }
     }
+    setInitialCounts();
 }
 
 
 
-// Timer Related Functions
-// Update the timer div with the current time
+
+
 function updateTimer() {
     let timerDiv = document.getElementById('timer');
     timerDiv.textContent = (hours < 10 ? "0" + hours : hours) + ":" +
@@ -208,40 +237,50 @@ function removeHighlight() {
 }
 
 
-function setGame() {
-    const counts = getNumberCounts(board);
 
-    // Digits 1-9
-    for (let i = 1; i <= 9; i++) {
-        let number = document.createElement("div");
-        number.id = i;
-        number.innerHTML = i + '<sup>' + (9 - counts[i]) + '</sup>'; // display count as superscript
-        number.addEventListener("click", selectNumber);
-        number.classList.add("number");
-        document.getElementById("digits").appendChild(number);
-    }
-
-    // Board 9x9
+function getNumberCount() {
+    let counts = Array(10).fill(0); // Indexes 0 to 9 (0 won't be used)
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            let tile = document.createElement("div");
-            tile.id = 'cell-' + r.toString() + "-" + c.toString();
-            if (board[r][c] != "-") {
-                tile.innerText = board[r][c];
-                tile.classList.add("tile-start");
+            const value = board[r][c];
+            if (value !== "-") {
+                counts[parseInt(value)]++;
             }
-            if (r == 2 || r == 5) {
-                tile.classList.add("horizontal-line");
-            }
-            if (c == 2 || c == 5) {
-                tile.classList.add("vertical-line");
-            }
-            tile.addEventListener("click", selectTile);
-            tile.classList.add("tile", "cell");
-            document.getElementById("board").append(tile);
+        }
+    }
+    return counts;
+}
+
+function setInitialCounts() {
+    const counts = getNumberCount();
+    for (let i = 1; i <= 9; i++) {
+        const remaining = 9 - counts[i];
+        updateNumberDisplay(i, remaining);
+    }
+}
+
+function updateNumberCount(number) {
+    const counts = getNumberCount();
+    const newCount = counts[number] + 1;
+    const remaining = 9 - newCount;
+    updateNumberDisplay(number, remaining);
+}
+
+function updateNumberDisplay(number, remaining) {
+    const numberElement = document.getElementById(number.toString());
+    if (numberElement) {
+        const superscript = numberElement.querySelector('.superscript');
+        if (superscript) {
+            superscript.textContent = remaining;
+        } else {
+            const newSuperscript = document.createElement('sup');
+            newSuperscript.className = 'superscript';
+            newSuperscript.textContent = remaining;
+            numberElement.appendChild(newSuperscript);
         }
     }
 }
+
 
 function getNumberCounts(board) {
     const counts = {
@@ -266,3 +305,4 @@ function getNumberCounts(board) {
 
     return counts;
 }
+
