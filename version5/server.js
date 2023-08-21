15bdd4f0bf284b18542b14059c257bf51a0aa2cd
gato365 <immanuelwilliams@gato365.com>
2023-08-21 16:59:22 -0700

    Set up server

---
+++ version5/server.js
@@ -0,0 +1,23 @@
+// Initiate the server
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// Set up port and app
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Serve up static assets
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Here is the link http://localhost:${PORT}`)
+    });
+
+});
